46c1243dcb4cd2928a22e41451a2c2e07b92e952
Nuatu <student@epicodus.com>
2014-09-05 17:09:32 -0700

    Update UI.

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -1,6 +1,9 @@
 class LanguagesController < ApplicationController
 
   def index
+    @language = Language.new
+    @languages = Language.all
+    @tags = Tag.all
     render('/languages/index.html.erb')
   end
 end
